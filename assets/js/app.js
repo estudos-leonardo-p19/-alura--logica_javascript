656ed87fa43978f23f2f27254cc35f17125ccee8
Leonardo Pimentel <leonardo.pimentel.santos.br@gmail.com>
2024-06-30 18:51:24 -0300

    WIP: Incrementando com contator

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,18 @@
 alert('Hallo! Boas vindas ao Jogo do Número Secreto');
 let secretNumber = 2;
-let attemps = prompt('Escolha um número entre 1 e 10') 
+let divination; 
+let attemps = 1;
 
-if( attemps == secretNumber){
-    alert(`Parabéns, você acertou" O n[umero secreto é: ${secretNumber}`)
-}else{
-    alert('Você errou')
+while(divination != secretNumber){
+    divination = prompt('Escolha um número entre 1 e 10');
+    if( divination == secretNumber){
+        alert(`Parabéns, você acertou" O número secreto é: ${secretNumber} e você precisou de ${attemps} tentativas`);
+    }else{
+        if(divination > secretNumber){
+            alert('Quase! O numero secreto é menor que ' + divination);
+        }else{
+            alert('O número Secreto é maior do que ' + divination);    
+        }
+        attemps ++;
+    }
 }
