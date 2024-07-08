46803f1b5251d1ff6502292b13dc8e8045755c45
Leonardo Pimentel <leonardo.pimentel.santos.br@gmail.com>
2024-07-08 18:51:06 -0300

    WIP: Operador Ternário

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,13 +6,23 @@ let attemps = 1;
 while(divination != secretNumber){
     divination = prompt('Escolha um número entre 1 e 10');
     if( divination == secretNumber){
-        alert(`Parabéns, você acertou" O número secreto é: ${secretNumber} e você precisou de ${attemps} tentativas`);
+        break;
     }else{
         if(divination > secretNumber){
             alert('Quase! O numero secreto é menor que ' + divination);
         }else{
             alert('O número Secreto é maior do que ' + divination);    
         }
-        attemps ++;
+        attemps++;
     }
 }
+
+let word = attemps > 1 ? 'tentativas' : 'tentativa';
+alert(`Parabéns, você acertou O número secreto é: ${secretNumber} e você precisou de ${attemps} ${word}`);
+
+
+// if(attemps > 1){
+    
+// }else{
+//     alert(`Parabéns, você acertou O número secreto é: ${secretNumber} e você precisou de ${attemps} tentativa`);
+// }
